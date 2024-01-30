0a78880406117189b94e3000ec61462e1a3dd5c8
Tetro <itsofirk@gmail.com>
2024-01-30 19:31:01 +0200

    created a setup.py file and the tar_reader package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tar_reader',
+    version='1.0.0',
+    description='A library for converting RAW images in a tar file to PNG images',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies if needed
+    ],
+)
